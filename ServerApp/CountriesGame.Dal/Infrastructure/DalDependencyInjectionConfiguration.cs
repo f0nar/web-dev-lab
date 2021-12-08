74eb09f1b69b47782a06972b4da6abe815fac6fc
Bogdan Selentiy <Bogdan.Selentiy@livenau.net>
2021-12-08 23:05:28 +0200

    Added DI config for DAL.

---
+++ ServerApp/CountriesGame.Dal/Infrastructure/DalDependencyInjectionConfiguration.cs
@@ -0,0 +1,31 @@
+using CountriesGame.Dal.DbContext;
+using CountriesGame.Dal.Entities;
+using CountriesGame.Dal.UnitOfWorks;
+using CountriesGame.Dal.UnitOfWorks.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CountriesGame.Dal.Infrastructure
+{
+    public static class DalDependencyInjectionConfiguration
+    {
+        public static IServiceCollection ConfigureDal(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            string connectionString = configuration["ConnectionStrings:DefaultConnection"];
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddIdentityCore<User>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddTransient<IUnitOfWork, EFUnitOfWork>();
+
+            return services;
+        }
+    }
+}
