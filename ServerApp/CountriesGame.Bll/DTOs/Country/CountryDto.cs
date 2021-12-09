28d0fbaa5fc81e9180f28fd43ccee9685e3d4322
Bogdan Selentiy <Bogdan.Selentiy@livenau.net>
2021-12-09 23:45:15 +0200

    Removed path properties.

--- ServerApp/CountriesGame.Bll/DTOs/Country/CountryDto.cs
+++ ServerApp/CountriesGame.Bll/DTOs/Country/CountryDto.cs
@@ -10,12 +10,6 @@ namespace CountriesGame.Bll.DTOs
 
         public string CapitalName { get; set; }
 
-        public string FlagPath { get; set; }
-
-        public string CoatPath { get; set; }
-
-        public string AnthemPath { get; set; }
-
         public IEnumerable<LinkDto> Links { get; set; }
     }
 }
