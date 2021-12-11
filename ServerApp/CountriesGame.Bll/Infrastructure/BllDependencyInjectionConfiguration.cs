26fa73d5b39f7c7c593bc3d1423b2bad7f885d92
Bogdan Selentiy <Bogdan.Selentiy@livenau.net>
2021-12-11 22:36:19 +0200

    Configuring DI for BLL.

---
+++ ServerApp/CountriesGame.Bll/Infrastructure/BllDependencyInjectionConfiguration.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Configuration;
+using CountriesGame.Dal.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using CountriesGame.Bll.Services.Interfaces;
+using CountriesGame.Bll.Services;
+
+namespace CountriesGame.Bll.Infrastructure
+{
+    public static class BllDependencyInjectionConfiguration
+    {
+        public static IServiceCollection ConfigureBll(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.ConfigureDal(configuration);
+
+            services.AddAutoMapper(typeof(BetweenDalAndBllMapperProfile));
+
+            services.Configure<AuthOptions>(configuration.GetSection("AuthOptions"));
+
+            services.AddTransient<IAuthService, AuthService>();
+            services.AddTransient<ICountryService, CountryService>();
+            services.AddTransient<ILinkService, LinkService>();
+            services.AddTransient<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
