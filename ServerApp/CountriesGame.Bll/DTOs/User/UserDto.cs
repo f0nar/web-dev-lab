f06c6575291f928e4917f59a13eed636945bacad
Bogdan Selentiy <Bogdan.Selentiy@livenau.net>
2021-12-13 14:56:22 +0200

    Added UserName property.

--- ServerApp/CountriesGame.Bll/DTOs/User/UserDto.cs
+++ ServerApp/CountriesGame.Bll/DTOs/User/UserDto.cs
@@ -6,6 +6,8 @@ namespace CountriesGame.Bll.DTOs
     {
         public string Id { get; set; }
 
+        public string UserName { get; set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
